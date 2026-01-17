10f005b43eb67ad379cc789ad30ed359884eba76
MDKod <mdkod.7@gmail.com>
2026-01-17 21:45:19 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+ 🎬 Movie App
+
+Movie App, **The Movie Database (TMDb) API** kullanarak popüler filmleri listeleyen ve kullanıcıların film araması yapmasına imkan veren basit bir web uygulamasıdır.  
+
+
+ ⚡ Özellikler
+- 🍿 Popüler filmleri listeleme  
+- 🔍 Film arama özelliği  
+- 🎨 Film puanına göre renkli etiketleme:
+  - 🟢 Yeşil: 8 ve üzeri  
+  - 🟠 Turuncu: 5-7.9  
+  - 🔴 Kırmızı: 5’in altında  
+- 📝 Film detay özetini gösterme  
+- 🖼 Poster resmi olmayan filmler için placeholder  
+
+---
+
+🛠️ Kullanılan Teknolojiler
+- 💻 HTML, CSS, JavaScript (Vanilla JS)  
+- 🌐 Fetch API  
+- 🎥 TMDb API  
+
+
+📷
+![ekran](EKRAN.jpg)
+
+<br>
+
+![gif](FİLM.jpg)
+
+
---
+++ app.js
@@ -0,0 +1,73 @@
+const API_URL = `https://api.themoviedb.org/3/movie/popular?api_key=a6822ddc5d3df5cc15742bb7dbf7508e&language=tr-TR`;
+const IMG_PATH = "https://image.tmdb.org/t/p/w500";
+const SEARCH_URL =
+  "https://api.themoviedb.org/3/search/movie?api_key=a6822ddc5d3df5cc15742bb7dbf7508e&language=tr-TR&query=";
+
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+const main = document.getElementById("main");
+
+getMovies(API_URL);
+
+async function getMovies(url) {
+  const res = await fetch(url);
+  const data = await res.json();
+
+  console.log(data.results);
+  showMovies(data.results);
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const searchTerm = search.value;
+
+  if (searchTerm && searchTerm !== "") {
+    getMovies(SEARCH_URL + searchTerm);
+
+    search.value = "";
+  } else {
+    window.location.reload();
+  }
+});
+
+function showMovies(movies) {
+  main.innerHTML = "";
+
+  movies.forEach((movie) => {
+    const { title, poster_path, vote_average, overview } = movie;
+
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie");
+
+    movieEl.innerHTML = `
+         <img
+          src="${IMG_PATH + poster_path}"
+          alt="${title}"
+        />
+
+        <div class="movie-info">
+          <h3>${title}</h3>
+          <span class="green">${vote_average}</span>
+        </div>
+
+        <div class="overview">
+          <h3>${title} <small>overview</small></h3>
+          <p>
+            ${overview}
+          </p>
+        </div> `;
+
+    main.appendChild(movieEl);
+  });
+}
+
+function getClassByRate(vote) {
+  if (vote >= 8) {
+    return "green";
+  } else if (vote >= 5) {
+    return "orange";
+  } else {
+    return "red";
+  }
+}
